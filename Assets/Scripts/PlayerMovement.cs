56d3dc21d8ef0639a6950658513c8ce83e58c88c
Andreas <andreasohlin.android@gmail.com>
2020-10-30 11:38:26 +0100

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float _speed = 5f;
+
+    private Vector2 _movementInput = Vector2.zero;
+    private Rigidbody2D _rb = null;
+	
+	void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movementInput.x = Input.GetAxisRaw("Horizontal");
+        _movementInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+	private void FixedUpdate()
+	{
+        _rb.MovePosition(_rb.position + (_movementInput.normalized) * _speed * Time.fixedDeltaTime);
+	}
+}
